db562866bb4989044e053012176f273f768e1556
Terra <you@example.com>
2021-10-25 12:22:49 -0400

    have score tag and title and font installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
+  let activeScore = 0;
+  // const leaderBoard;
+
   return (
     <div className="App">
+      <h1>2048</h1>
+      
+      <div class="score-Continer">
+        <div class="score-Title">Score:</div>
+        <span id="score">{activeScore}</span>
+      </div>
+
       <div class="gamingGrid">
-        
+
       </div>
     </div>
   );
